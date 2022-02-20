b349a70487d7373939028e10603d7c186d8cdecd
Kevin Williams <kevin@k9w.org>
2022-02-20 12:31:05 -0800

    Add functions to convert between inches and centimeters.

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,28 @@ const celsiusOutput = fahrenheitInput + ' degrees F is ' + fahrenheitToCelsius(f
 alert(fahrenheitOutput + '\n' + celsiusOutput);
 
 */
+
+function inchesToCentimeters(inchesInput) {
+  return inchesInput * 2.54;
+}
+
+function centimetersToInches(centimetersInput) {
+  return centimetersInput / 2.54;
+}
+
+const inchesInput = parseInt(prompt('Enter inches to convert to centimeters.'));
+const centimetersOutput = inchesInput + ' inches is ' + inchesToCentimeters(inchesInput) + ' centimeters.';
+
+alert(centimetersOutput);
+
+const centimetersInput = parseInt(prompt('Enter centimeters to convert to inches.'));
+const inchesOutput = centimetersInput + ' centimeters is ' + centimetersToInches(centimetersInput) + ' inches.';
+
+alert(inchesOutput);
+
+// Converting from centimeters to inches (dividing by 2,54) does not
+// return the inverse result of inches to centimeters, (multiply by
+// 2.54). This issue does not show up in Google's converter. It's
+// likely specific to JavaScript and can be improved by a helper
+// method.
+
